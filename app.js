2040139d9fa03027dc2f7f02fda8c84ad4efe8cb
mylastore <mrtonyq22@gmail.com>
2022-10-22 20:49:16 -0700

    adjust dev vs live servers

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 
 require('dotenv').config()
 const https = require('https')
+const http = require('http')
 const fs = require('fs')
 
-const env = process.env.NODE_ENV
+const isDev = (process.env.DEV_ENV === 'development')
+console.log(process.env.NODE_ENV)
+console.log(isDev)
 const port = process.env.PORT
-const src = env === 'production' ? './build/index' : './src/index'
+const src = isDev ? './src/index' : './build/index'
 
 const options = {
   key: fs.readFileSync(process.env.LOCAL_KEY_PEM),
@@ -15,7 +18,7 @@ const options = {
 
 // setting to support to used import instead of required
 require('@babel/polyfill')
-if (env === 'development') {
+if (isDev) {
   require('@babel/register')
 }
 
@@ -23,7 +26,9 @@ const app = require(src).default
 
 //Here we're assigning the server to a variable because
 //we're going to want to manually rip down the server in testing
-const server = https.createServer(options, app.callback()).listen(port)
+
+const server = isDev ? https.createServer(options, app.callback()).listen(port) :
+  http.createServer().listen(port)
 
 console.log('https://localhost:' + port)
 console.log("Running in " + process.env.NODE_ENV + " v" + process.env.NPM_PACKAGE_VERSION)
