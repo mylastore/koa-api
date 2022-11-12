318fe8f38019b8a5a9384f6ceb8fb85667772b91
mylastore <mrtonyq22@gmail.com>
2022-11-12 15:38:13 -0800

    https issues

--- app.js
+++ app.js
@@ -6,24 +6,21 @@ const fs = require('fs')
 const isDev = process.env.NODE_ENV === 'development'
 const port = process.env.PORT
 const src = isDev ? './src/index' : './build/index'
-let options = {}
-
 
 // setting to support to used import instead of required
 require('@babel/polyfill')
 if (isDev) {
   require('@babel/register')
-  options = {
-    key: isDev ? fs.readFileSync(process.env.LOCAL_KEY) : fs.readFileSync(process.env.LIVE_KEY),
-    cert: isDev ? fs.readFileSync(process.env.LOCAL_CERT) : fs.readFileSync(process.env.LIVE_CERT),
-  }
 }
 
+const options = {
+  key: isDev ? fs.readFileSync(process.env.LOCAL_KEY) : fs.readFileSync(process.env.LIVE_KEY),
+  cert: isDev ? fs.readFileSync(process.env.LOCAL_CERT) : fs.readFileSync(process.env.LIVE_CERT),
+}
 const app = require(src).default
 
 //Here we're assigning the server to a variable because
 //we're going to want to manually rip down the server in testing
-
 const server = https.createServer(options, app.callback()).listen(port)
 
 console.log('https://localhost:' + port)
