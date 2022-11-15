5a61a4a28a5d2c509bb90add4c48ebb5b8cd87e6
mylastore <mrtonyq22@gmail.com>
2022-11-14 21:00:04 -0800

    remove unique from email user mongo model

--- src/models/User.js
+++ src/models/User.js
@@ -21,7 +21,6 @@ const userSchema = new mongoose.Schema(
         email: {
             type: String,
             trim: true,
-            unique: true,
             lowercase: true,
             match: [
                 /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
